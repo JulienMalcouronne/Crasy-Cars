a96859ee885181916d02de1ae013e91000d89314
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-19 10:15:52 +0100

    unfinished

--- app/javascript/plugins/listener.js
+++ app/javascript/plugins/listener.js
@@ -1,8 +1,22 @@
-const button = document.querySelector('.button-home');
-const audio = new Audio('sound.mp3');
+// const buttonListener = () => {
+  const buttonDashToConfirmed = document.querySelectorAll("#confirm");
+  console.log(buttonDashToConfirmed)
+  buttonDashToConfirmed.addEventListener('click', (event) => {
+    event.currentTarget.classList.remove('btn-warning')
+    event.currentTarget.classList.add('btn-success')
+  });
+// };
+export { buttonDashToConfirmed };
+// export { buttonListener };
 
-button.addEventListener('click', (e) => {
-  e.target.classList.add('disabled');
-  e.target.innerText = 'Bingo!';
-  audio.play();
-});
+
+
+
+// const button = document.querySelector('.button-home');
+// const audio = new Audio('sound.mp3');
+
+// button.addEventListener('click', (e) => {
+//   e.target.classList.add('disabled');
+//   e.target.innerText = 'Bingo!';
+//   audio.play();
+// });
